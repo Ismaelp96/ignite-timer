1dfe4fcf22c850274020581df5c123f2fa97b2fa
ismaelp96 <ismael6991@live.com>
2025-05-02 14:26:13 -0300

    feat: React Hook Form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
+import { useForm } from 'react-hook-form';
 import { Play } from '@phosphor-icons/react';
 import * as S from './styles';
 
 export function Home() {
+	const { register, handleSubmit, watch } = useForm();
+
+	function handleCreateNewCycle(data: any) {
+		console.log(data);
+	}
+
+	const task = watch('task');
+	const isSubmitDisabled = !task;
 	return (
 		<S.HomeContainer>
-			<form>
+			<form onSubmit={handleSubmit(handleCreateNewCycle)}>
 				<S.TitleContainer>
 					<label htmlFor='task'>Vou trabalhar em</label>
 					<S.TaskInput
@@ -12,6 +21,7 @@ export function Home() {
 						id='task'
 						list='task-suggestions'
 						placeholder='Dê um nome para o seu projeto'
+						{...register('task')}
 					/>
 					<datalist id='task-suggestions'>
 						<option value='Porjeto 1' />
@@ -26,6 +36,7 @@ export function Home() {
 						step={5}
 						min={5}
 						max={60}
+						{...register('minutesAmount', { valueAsNumber: true })}
 					/>
 					<span>minutos</span>
 				</S.TitleContainer>
@@ -36,7 +47,7 @@ export function Home() {
 					<span>0</span>
 					<span>0</span>
 				</S.CountDownContainer>
-				<S.StartCountdownButton type='submit'>
+				<S.StartCountdownButton type='submit' disabled={isSubmitDisabled}>
 					<Play />
 					Começar
 				</S.StartCountdownButton>
